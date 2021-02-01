2fbbfa70735ef0c1dcbff8851b919436d5697e78
Zacchaeus Napuo <2016.naaz@gmail.com>
2021-02-02 04:53:05 +0530

    Updates

---
+++ precache-manifest.b54f074b39dcd63e74154e211729cc7c.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef9ecdc320c7c0f8a2e9961bf2ff078",
+    "url": "/ZacchNaa/zprofile/index.html"
+  },
+  {
+    "revision": "e7e9734d563e104be262",
+    "url": "/ZacchNaa/zprofile/static/css/main.54b3de93.chunk.css"
+  },
+  {
+    "revision": "3c25d0f8eed81b585933",
+    "url": "/ZacchNaa/zprofile/static/js/2.d2640e18.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/ZacchNaa/zprofile/static/js/2.d2640e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e9734d563e104be262",
+    "url": "/ZacchNaa/zprofile/static/js/main.a5662cda.chunk.js"
+  },
+  {
+    "revision": "7ba677d9b964a174d5cc",
+    "url": "/ZacchNaa/zprofile/static/js/runtime-main.c152bd98.js"
+  },
+  {
+    "revision": "bc4f0a0afc591586e9169f5fe48be9c4",
+    "url": "/ZacchNaa/zprofile/static/media/Artboard1.bc4f0a0a.webp"
+  },
+  {
+    "revision": "e0c0b981ed5ab8c914e554f3ad872c33",
+    "url": "/ZacchNaa/zprofile/static/media/Artboard2.e0c0b981.webp"
+  },
+  {
+    "revision": "de124460263e3d778cfc932a77cb8441",
+    "url": "/ZacchNaa/zprofile/static/media/Artboard3.de124460.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/ZacchNaa/zprofile/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "fde6bd37ae4a55a9fa95c0b3d1534f6a",
+    "url": "/ZacchNaa/zprofile/static/media/bg3.fde6bd37.webp"
+  },
+  {
+    "revision": "2d72017147087cfec2af7c10d92b62cc",
+    "url": "/ZacchNaa/zprofile/static/media/bg9.2d720171.webp"
+  },
+  {
+    "revision": "a1bc3b4829feaa1e101a2a153a1f1ee2",
+    "url": "/ZacchNaa/zprofile/static/media/bookcover1.a1bc3b48.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/ZacchNaa/zprofile/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "be8f33b6a5cf94e6fd2acdccbe0dde44",
+    "url": "/ZacchNaa/zprofile/static/media/chatapp.be8f33b6.webp"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/ZacchNaa/zprofile/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/ZacchNaa/zprofile/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/ZacchNaa/zprofile/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/ZacchNaa/zprofile/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/ZacchNaa/zprofile/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/ZacchNaa/zprofile/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/ZacchNaa/zprofile/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/ZacchNaa/zprofile/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/ZacchNaa/zprofile/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "b446ee3a31ad8fe8a363e1ee0fc96e88",
+    "url": "/ZacchNaa/zprofile/static/media/img19.b446ee3a.webp"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/ZacchNaa/zprofile/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "f514460f70a26953a1c8760a008ace37",
+    "url": "/ZacchNaa/zprofile/static/media/logo.f514460f.svg"
+  },
+  {
+    "revision": "f9314a2bf8035c8826f8d0cef1b61136",
+    "url": "/ZacchNaa/zprofile/static/media/logow3.f9314a2b.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/ZacchNaa/zprofile/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/ZacchNaa/zprofile/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/ZacchNaa/zprofile/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/ZacchNaa/zprofile/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/ZacchNaa/zprofile/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "40aba4a4c3ed473ed7f0e4abc61665a1",
+    "url": "/ZacchNaa/zprofile/static/media/profile.40aba4a4.webp"
+  },
+  {
+    "revision": "ac36ece318566f596e6d1d810cdcec81",
+    "url": "/ZacchNaa/zprofile/static/media/profile.ac36ece3.svg"
+  },
+  {
+    "revision": "0f885ab8bdefc27d1d016e363c8ed8a4",
+    "url": "/ZacchNaa/zprofile/static/media/project1.0f885ab8.webp"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/ZacchNaa/zprofile/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/ZacchNaa/zprofile/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/ZacchNaa/zprofile/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/ZacchNaa/zprofile/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "05fc03ae75ef4bb1e7649dea17211773",
+    "url": "/ZacchNaa/zprofile/static/media/slide1.05fc03ae.webp"
+  },
+  {
+    "revision": "2a6be8ab1b96ca2ae3dad9f9a4f98741",
+    "url": "/ZacchNaa/zprofile/static/media/slide1.2a6be8ab.webp"
+  },
+  {
+    "revision": "2b0eb6e38d9fdc920409c3e22253f5a1",
+    "url": "/ZacchNaa/zprofile/static/media/slide1.2b0eb6e3.webp"
+  },
+  {
+    "revision": "5be0180a128860c4b75db1ff7c1c6acf",
+    "url": "/ZacchNaa/zprofile/static/media/slide1.5be0180a.webp"
+  },
+  {
+    "revision": "c30f391e791e632e61a30861b02729f9",
+    "url": "/ZacchNaa/zprofile/static/media/slide1.c30f391e.webp"
+  },
+  {
+    "revision": "23e4ea556de58464634dd5ceba85f8ea",
+    "url": "/ZacchNaa/zprofile/static/media/slide2.23e4ea55.webp"
+  },
+  {
+    "revision": "3fea39b73c90059b3e04753fffd1ce82",
+    "url": "/ZacchNaa/zprofile/static/media/slide2.3fea39b7.webp"
+  },
+  {
+    "revision": "7b7ceba8bf27cb3374024cbec745849d",
+    "url": "/ZacchNaa/zprofile/static/media/slide2.7b7ceba8.webp"
+  },
+  {
+    "revision": "a1bc3b4829feaa1e101a2a153a1f1ee2",
+    "url": "/ZacchNaa/zprofile/static/media/slide2.a1bc3b48.webp"
+  },
+  {
+    "revision": "d809cf004d863ce10e722048792dd20e",
+    "url": "/ZacchNaa/zprofile/static/media/slide2.d809cf00.png"
+  },
+  {
+    "revision": "25dbda5969eb25a0e3386ad3501914f1",
+    "url": "/ZacchNaa/zprofile/static/media/slide3.25dbda59.webp"
+  },
+  {
+    "revision": "68fa9f70e9397bc2b8c0695af3bb86d3",
+    "url": "/ZacchNaa/zprofile/static/media/slide3.68fa9f70.webp"
+  },
+  {
+    "revision": "9d759cd3db4da0747bbf375e2766a660",
+    "url": "/ZacchNaa/zprofile/static/media/slide3.9d759cd3.webp"
+  },
+  {
+    "revision": "d3300bf47c019bec87a099f0b2e21c55",
+    "url": "/ZacchNaa/zprofile/static/media/slide3.d3300bf4.webp"
+  },
+  {
+    "revision": "d6cc9e76262f320616b6ceb4a1808ce7",
+    "url": "/ZacchNaa/zprofile/static/media/slide3.d6cc9e76.webp"
+  },
+  {
+    "revision": "195af0412478e5846c103b5f1f86919e",
+    "url": "/ZacchNaa/zprofile/static/media/slide4.195af041.webp"
+  },
+  {
+    "revision": "5b07368a15c286e7fa997a346b33adce",
+    "url": "/ZacchNaa/zprofile/static/media/slide4.5b07368a.webp"
+  },
+  {
+    "revision": "5d2f69c2b29dd6d8bccec35337f68e6f",
+    "url": "/ZacchNaa/zprofile/static/media/slide4.5d2f69c2.webp"
+  },
+  {
+    "revision": "a1ac494c189011a1579a6600d867c2b4",
+    "url": "/ZacchNaa/zprofile/static/media/slide4.a1ac494c.webp"
+  },
+  {
+    "revision": "68fa9f70e9397bc2b8c0695af3bb86d3",
+    "url": "/ZacchNaa/zprofile/static/media/slide5.68fa9f70.webp"
+  },
+  {
+    "revision": "69a8c5042c1b0829a225db2f241e9a76",
+    "url": "/ZacchNaa/zprofile/static/media/slide5.69a8c504.webp"
+  },
+  {
+    "revision": "e7cb6d1a5e3c6b0dff4ae22039ad7f5d",
+    "url": "/ZacchNaa/zprofile/static/media/slide5.e7cb6d1a.webp"
+  },
+  {
+    "revision": "cb249c11418012b5c256dc3b23313582",
+    "url": "/ZacchNaa/zprofile/static/media/slide6.cb249c11.webp"
+  },
+  {
+    "revision": "cc94c851d7f70c27ba42967158364b82",
+    "url": "/ZacchNaa/zprofile/static/media/slide7.cc94c851.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/ZacchNaa/zprofile/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "24391d48430ab7fdd35145e0af152cd6",
+    "url": "/ZacchNaa/zprofile/static/media/todoapp.24391d48.webp"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/ZacchNaa/zprofile/static/media/typescript.3de182d2.svg"
+  }
+]);
